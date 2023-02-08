711afc884c3c378abeffea03321f3cfbb7b88bf5
satyarathi <jitendrasatyarathi@gmail.com>
2023-02-08 20:17:05 +0530

    [Jitendra]Added: Synchronous and asynchronous

---
+++ NonBlocking/nonblocking.js
@@ -0,0 +1,14 @@
+// Synchronous
+
+console.log('First');
+console.log('Second');
+console.log('Third');
+
+// Asynchronous
+
+console.log('First');
+
+// Setting timeout for 4 seconds
+setTimeout(() => console.log('Second'), 4000);
+
+console.log('Third');
