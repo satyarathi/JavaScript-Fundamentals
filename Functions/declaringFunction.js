83dc536efcdda4a6b51c66e9d907101fff06fccf
satyarathi <jitendrasatyarathi@gmail.com>
2023-02-08 20:09:34 +0530

    [Jitendra]Added: Function Declaration

---
+++ Functions/declaringFunction.js
@@ -0,0 +1,32 @@
+// let x = add(5, 2);
+
+// function add(a, b) {
+//     return a + b;
+// }
+// add(5, 2);
+
+// console.log(x);
+
+// function myFunction() {
+//     console.log("Hello World!");
+// }
+// myFunction();
+
+// Arrow function
+
+// let x = sum(2)
+
+// function sum(a) {
+//     return a + 100;
+
+// }
+
+// console.log(sum(2));
+
+// let x = (a) => {
+//     return a + 100;
+// }
+// console.log(x(2));
+
+let x = a => a + 100;
+console.log(x(5));
